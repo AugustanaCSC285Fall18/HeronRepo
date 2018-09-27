f4984b7fe7b2f0416856a9c0c64a6c5c3d55448d
Yafet Zeleke <yafetzeleke17@augustana.edu>
2018-09-27 13:44:43 -0500

    actionListener working.

--- ProjectHeron/src/edu/augustana/csc285/heron/MainWindowController.java
+++ ProjectHeron/src/edu/augustana/csc285/heron/MainWindowController.java
@@ -1,6 +1,9 @@
 package edu.augustana.csc285.heron;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -20,6 +23,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import javafx.event.EventHandler;
 
 public class MainWindowController {
 	@FXML
@@ -29,11 +36,12 @@ public class MainWindowController {
 
 	private VideoCapture capture = new VideoCapture();
 	private ScheduledExecutorService timer;
-
 	
 	public Image grabFrame() {
 		Mat frame = new Mat();
-		capture.read(frame);		
+		capture.read(frame);
+		capture.get(Videoio.CAP_PROP_FRAME_COUNT);
+		System.out.println(capture.get(Videoio.CAP_PROP_FRAME_COUNT));
 		MatOfByte buffer = new MatOfByte();
 		Imgcodecs.imencode(".png", frame, buffer);
 		Image imageToShow = new Image(new ByteArrayInputStream(buffer.toArray()));
@@ -47,11 +55,28 @@ public class MainWindowController {
 
 		return imageToShow;
 	}
-
+	
+	
 	@FXML
 	protected void startButton(ActionEvent event) {
-		capture.open("S:\\CLASS\\CS\\285\\sample_videos\\sample1.mp4");
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Video File");
+		Window mainWindow = videoFrame.getScene().getWindow();
+		File chosenFile = fileChooser.showOpenDialog(mainWindow);
+		String filename = chosenFile.getAbsolutePath();
 
+		if (chosenFile != null) {
+			try {
+
+				videoFrame.setImage(new Image(new FileInputStream(chosenFile)));
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			capture.open(filename);
+		}
+		
 		Runnable frameGrabber = new Runnable() {
 			public void run() {
 				// get one frame from the video
@@ -75,12 +100,15 @@ public class MainWindowController {
 
 		this.timer = Executors.newSingleThreadScheduledExecutor();
 		this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
-
-	}
-	
-	public void mouseClicked (ActionEvent e ) {
 		
+		EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() { 
+	         @Override 
+	         public void handle(MouseEvent e) {
+	        	 System.out.println("x: " + e.getX() + ", y: " + e.getY());
+	         }
+		};
+		
+		videoFrame.setOnMouseClicked(eventHandler);
 	}
-	
-	
+			
 }
